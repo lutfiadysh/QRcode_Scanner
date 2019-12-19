75e240796fd8d570b1a4ad02516ae81279bb2f05
lutfiadysh <lutfi020117@gmail.com>
2019-12-19 09:06:49 +0700

    tambah alert

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,8 @@ import 'package:barcode_scan/barcode_scan.dart';
 import 'package:flutter/services.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'data.dart';
+import 'package:splashscreen/splashscreen.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
 final FirebaseApp app = FirebaseApp();
 
@@ -15,8 +16,21 @@ void main() => runApp(new MaterialApp(
     fontFamily: 'arial',
   ),
   debugShowCheckedModeBanner: false,
-  home: HomeScreen(),
-));
+  home: new SplashScreen(seconds: 3,navigateAfterSeconds: HomeScreen(),
+      title: new Text('SHUN QRcode scanner',
+        style: new TextStyle(
+            fontFamily: 'dark',
+            fontWeight: FontWeight.bold,
+            fontSize: 20.0
+        ),),
+      backgroundColor: Colors.lightBlue,
+      styleTextUnderTheLoader: new TextStyle(),
+      photoSize: 100.0,
+      onClick: ()=>print("Lutfi Ardianysah"),
+      loaderColor: Colors.red
+      ),
+    ),
+  );
 
 class HomeScreen extends StatefulWidget {
 
@@ -36,9 +50,9 @@ class _HomeScreenState extends State<HomeScreen> {
   void initState()
   {
     super.initState();
-    item = Item("");
+    item = Item("","");
     final FirebaseDatabase database = FirebaseDatabase.instance;
-    itemRef = database.reference().child('data');
+    itemRef = database.reference().child('data/shun');
     itemRef.onChildAdded.listen(_onEntryAdded);
     itemRef.onChildChanged.listen(_onEntryChanged);
   }
@@ -46,6 +60,22 @@ class _HomeScreenState extends State<HomeScreen> {
   _onEntryAdded(Event event) {
     setState(() {
       items.add(Item.fromSnapshot(event.snapshot));
+      Alert(
+        context: context,
+        title: "Berhasil",
+        desc: "Data berhasil tersimpan.",
+        buttons: [
+          DialogButton(
+            child: Text(
+              "OK",
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+            onPressed: () => Navigator.pop(context),
+            color: Color.fromRGBO(0, 179, 134, 1.0),
+            radius: BorderRadius.circular(0.0),
+          ),
+        ],
+      ).show();
     });
   }
 
@@ -97,98 +127,104 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
     return Scaffold(
       key:scaffoldKey,
       appBar: AppBar(
         centerTitle: true,
         title: Text('SHUN QRCode Scanner',style: TextStyle(fontFamily: 'dark'),),
+        actions: <Widget>[
+          IconButton(
+            icon:Icon(Icons.info_outline),
+            onPressed: () {
+
+            },
+          ),
+        ],
       ),
-      body: Center(
-          child:
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.fromLTRB(200.0, 20.0, 200.0, 20.0),
-                width: 5,
-                child:Form(
-                  key: formKey,
-                child: Column(
-                  children: <Widget>[
-                    new GestureDetector(
-                      child:Text(result,
-                      style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
-                      ),
-                      onLongPress: () {
-                        Clipboard.setData(new ClipboardData(text: result));
-                        scaffoldKey.currentState.showSnackBar(
-                            new SnackBar(content: new Text("Berhasil di Copy!")));
-                      },
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    new TextFormField(
-                      initialValue: "",
-                      onSaved: (val) => item.title = val,
-                      validator: (val) => val == "" ? val : null,
-                      decoration: InputDecoration(
-                          labelText: 'Paste disini'
-                      ),
-                    ),
-                  ]),
+      body: ListView(
+        padding: EdgeInsets.fromLTRB(20.0, 40.0, 20.0, 20.0),
+              children: <Widget>[
+                Center(
+                  child:Form(
+                    key: formKey,
+                    child: Column(
+                        children: <Widget>[
+                          new GestureDetector(
+                            child:Text(result,
+                              style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
+                            ),
+                            onLongPress: () {
+                              Clipboard.setData(new ClipboardData(text: result));
+                              scaffoldKey.currentState.showSnackBar(
+                                  new SnackBar(content: new Text("Berhasil di Copy!")));
+                            },
+                          ),
+                          SizedBox(
+                            height: 40.0,
+                          ),
+                          new TextFormField(
+                            initialValue: "",
+                            onSaved: (val) => item.title = val,
+                            validator: (val) => val == "" ? val : null,
+                            decoration: InputDecoration(
+                                labelText: 'Paste disini'
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20.0,
+                          ),
+                          new TextFormField(
+                            initialValue: "",
+                            onSaved: (val) => item.name = val,
+                            validator: (val) => val == "" ? val : null,
+                            decoration: InputDecoration(
+                                labelText: 'NIS Anda'
+                            ),
+                          ),
+                          SizedBox(
+                            height: 50.0,
+                          ),
+                        ]),
+                  ),
                 ),
-              ),
-              SizedBox(height: 200),
-              Center(
-              child : Column(
-                children: <Widget>[
-                  IconButton(
-                    onPressed: (){
-                      handleSubmit();
-                    },
-                    icon: Icon(Icons.send),
+                Center(
+                  child : Column(
+                    children: <Widget>[
+                      IconButton(
+                        onPressed: (){
+                          handleSubmit();
+                        },
+                        icon: Icon(Icons.send),
+                      ),
+                    ],
                   ),
-                  MaterialButton(
-                    child: Text("Lihat Data!"),
-                    onPressed: (){
-                      Navigator.push(
-                          context,
-                      MaterialPageRoute(builder: (context) => DataHasil()));
-                    },
-                    height: 50,
-                    color: Colors.greenAccent,
-                    minWidth: 20,
-                    textColor: Colors.white,
-                    splashColor: Colors.green,
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Padding(padding: EdgeInsets.all(5),
+                  child:MaterialButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5)
+                      ),
+                      height: SizeConfig.safeBlockVertical * 5,
+                      minWidth: SizeConfig.safeBlockHorizontal * 5,
+                      color: Colors.orange,
+                      textColor: Colors.white,
+                      splashColor: Colors.green,
+                      onPressed: _scanQR,
+                      child: const Text('SCAN!')
                   ),
-                ],
-              ),
-              ),
-              SizedBox(
-                height: 5,
-              ),
-              Padding(padding: EdgeInsets.all(40),
-                child:MaterialButton(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5)
-                  ),
-                  height: 50,
-                  color: Colors.black26,
-                  minWidth: 20,
-                  textColor: Colors.white,
-                  splashColor: Colors.green,
-                  onPressed: _scanQR,
-                  child: const Text('SCAN!')
-              ),
-          ),
-              Center(
-                child: Text("Lutfi_Ardiansyah",
-                style: TextStyle(fontSize: 20,fontFamily: 'dark'),),
-              ),
-            ],
-          ),
+                ),
+                SizedBox(
+                  height: 100.0,
+                ),
+                Center(
+                  child: Text("Lutfi_Ardiansyah",
+                    style: TextStyle(fontSize: 20,fontFamily: 'dark'),),
+                ),
+              ],
       ),
     );
   }
@@ -196,17 +232,46 @@ class _HomeScreenState extends State<HomeScreen> {
 class Item {
   String key;
   String title;
-  String body;
+  String name;
 
-  Item(this.title);
+  Item(this.title,this.name);
 
   Item.fromSnapshot(DataSnapshot snapshot)
       : key = snapshot.key,
+        name = snapshot.value["name"],
         title = snapshot.value["title"];
+
 
   toJson() {
     return {
       "title": title,
+      "name": name,
     };
+  }
+}
+
+class SizeConfig {
+  static MediaQueryData _mediaQueryData;
+  static double screenWidth;
+  static double screenHeight;
+  static double blockSizeHorizontal;
+  static double blockSizeVertical;
+  static double _safeAreaHorizontal;
+  static double _safeAreaVertical;
+  static double safeBlockHorizontal;
+  static double safeBlockVertical;
+
+  void init(BuildContext context){
+    _mediaQueryData = MediaQuery.of(context);
+    screenWidth = _mediaQueryData.size.width;
+    screenHeight = _mediaQueryData.size.height;
+    blockSizeHorizontal = screenWidth/100;
+    blockSizeVertical = screenHeight/100;
+    _safeAreaHorizontal = _mediaQueryData.padding.left +
+        _mediaQueryData.padding.right;
+    _safeAreaVertical = _mediaQueryData.padding.top +
+        _mediaQueryData.padding.bottom;
+    safeBlockHorizontal = (screenWidth - _safeAreaHorizontal)/100;
+    safeBlockVertical = (screenHeight - _safeAreaVertical)/100;
   }
 }
